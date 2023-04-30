4dd08eb6c7cf411d47a10f1a3a8931b485d479fc
jrmouro <jronaldomouro@gmail.com>
2023-04-30 17:38:11 -0300

    score

--- Projeto/src/app/score.ts
+++ Projeto/src/app/score.ts
@@ -1,7 +1,8 @@
 export const ScoreType = {
-    response:"0",
-    pass:"1",
-    none:"2"
+    response:"0", //respondeu e ganhou
+    pass_response:"1",//não respondeu e ganhou quem passou
+    response_pass:"2",// respondeu questão passada
+    none:"3"
 } as const;
 
 export interface Score{
