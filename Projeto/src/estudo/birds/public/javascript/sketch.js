15ec3191e389de71a0f7643d8b9e5a3008076b54
jrmouro <jronaldomouro@gmail.com>
2023-07-07 11:36:31 -0300

    game def3 def4

--- Projeto/src/estudo/birds/public/javascript/sketch.js
+++ Projeto/src/estudo/birds/public/javascript/sketch.js
@@ -3,7 +3,7 @@ let game;
 function setup() {
 
   const canvas = createCanvas(800, 1400);
-  game = new Game(canvas.canvas.canvas, [scene1,scene2]);
+  game = new Game(canvas.canvas.canvas, [scene4, scene3, scene1, scene2]);
   game.setup();
 
 }
